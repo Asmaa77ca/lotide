8b194ad82f52ceae27d7b45e0eaf9b3915f9792e
Asmaa77ca <ao77@live.com>
2021-10-26 13:31:51 +0000

    tail exercise

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [actual] === [expected]`);
+  } else {
+    console.log(`🛑🛑 Assertion Failed: [actual] !== [expected]`);
+  }
+
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
+console.log(tail);
